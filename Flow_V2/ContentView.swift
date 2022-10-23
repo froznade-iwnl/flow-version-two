9d9454c091b855ffa0c75c3e8e3818c33c22b69d
Raditya <raditya.learner@gmail.com>
2022-10-23 11:26:11 +0800

    Initial Commit

---
+++ Flow_V2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Flow_V2
+//
+//  Created by CTSS Students on 23/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flow_V2/Flow_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Flow_V2App.swift
+//  Flow_V2
+//
+//  Created by CTSS Students on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Flow_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
